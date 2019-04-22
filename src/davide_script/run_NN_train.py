7575de728711567f0fb9b7f9a0ad5347af507ad7
AndreaBorghesi <andrea.borghesi3@unibo.it>
2019-04-22 11:22:20 +0200

    small changes

--- src/davide_script/run_NN_train.py
+++ src/davide_script/run_NN_train.py
@@ -5,13 +5,14 @@
 #SBATCH -N 1      # nodes requested
 #SBATCH
 #SBATCH -n 1      # tasks requested
-#SBATCH -c 1      # cores requested
+#SBATCH -c 4      # cores requested
 #SBATCH --mem=100000  # memory in Mb
 # --open-mode=append
 # --open-mode=truncate
-#SBATCH -o logs/outfile_mse_input_4gpu_bn_996imgsize_32bs_100zdim
-#SBATCH -e logs/errfile_mse_input_4gpu_bn_996imgsize_32bs_100zdim
-#SBATCH -t 03:00:00  # time requested in hour:minute:second
+#SBATCH -o logs/outfile_aae_4gpu_bn_996imgsize_32bs_2zdim
+#SBATCH -e logs/errfile_aae_4gpu_bn_996imgsize_32bs_2zdim
+#SBATCH -t 05:00:00  # time requested in hour:minute:second
 
 #python train_ae_davide_withImgGen.py
-python train_vae_davide_withImgGen.py
+#python train_vae_davide_withImgGen.py
+python train_aae_davide_withImgGen.py
--- src/davide_script/train_vae_davide_withImgGen.py
+++ src/davide_script/train_vae_davide_withImgGen.py
@@ -49,7 +49,7 @@ enhanced_contrast = 0
 
 _batch_size = 32
 _epochs = 100
-_latent_dim = 100
+_latent_dim = 50
 _cnn = True
 _vae_loss_kl_weight = 1
 _vae_loss_recon_weight = 1
@@ -61,10 +61,6 @@ if imgGen_class_mode == None:
     imgGen_class_mode_str = 'None'
 else:
     imgGen_class_mode_str = 'input'
-
-#train_loss = 'binary_crossentropy'
-#train_loss = 'mae'
-train_loss = 'mean_squared_error'
 
 n_gpu = 4
 
@@ -208,30 +204,24 @@ if _bn:
     bn_str = '_bn'
 else:
     bn_str = ''
-if train_loss == 'binary_crossentropy':
-    loss_str = 'bce'
-elif train_loss == 'mae':
-    loss_str = 'mae'
-elif train_loss == 'mean_squared_error':
-    loss_str = 'mse'
 
 #model_weights = ('{}model_weights_vae{}{}_{}imgSize_{}ep_{}bs_{}nbch_{}enhC_'
-#        '{}_{}_{}_{}zdim_{}recW_{}klW.h5'.format(trained_models_dir, cnn_str, 
+#        '{}_{}_{}zdim_{}recW_{}klW.h5'.format(trained_models_dir, cnn_str, 
 #            bn_str, img_target_size, _epochs, _batch_size, nb_channels, 
-#            enhanced_contrast, loss_str, imgGen_class_mode_str, n_gpu, 
+#            enhanced_contrast, imgGen_class_mode_str, n_gpu, 
 #            _latent_dim, _vae_loss_recon_weight, _vae_loss_kl_weight))
 model_weights = ('{}model_weights_vae{}{}_{}imgSize_{}ep_{}bs_{}nbch_{}enhC_'
-        '{}_{}_{}_{}zdim_{}recW_{}klW.hdf5'.format(
+        '{}_{}_{}zdim_{}recW_{}klW.hdf5'.format(
             trained_models_dir, cnn_str, 
             bn_str, img_target_size, _epochs, _batch_size, nb_channels, 
-            enhanced_contrast, loss_str, imgGen_class_mode_str, n_gpu, 
+            enhanced_contrast, imgGen_class_mode_str, n_gpu, 
             _latent_dim, _vae_loss_recon_weight, _vae_loss_kl_weight))
 
 checkpoint = ModelCheckpoint(filepath = model_weights,
         save_best_only=True,monitor="loss", mode="min" )
 history = History()
 
-vae.compile(optimizer='adam', loss=vae_loss, metrics=['val_loss'])
+vae.compile(optimizer='adam', loss=vae_loss)
 
 print("VAE{} Created & Compiled".format(cnn_str))
 print("\tModel saved weights file name {}".format(model_weights))
